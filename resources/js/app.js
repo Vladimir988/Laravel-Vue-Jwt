0baff9a7e42a66c3c8491d5f70abd0a52269c384
Vladimir988 <optimist0706@gmail.com>
2023-06-08 18:18:37 +0300

    setup SPA

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
+import Vue from 'vue';
+import Index from "./components";
+import router from "./router";
 require('./bootstrap');
+
+const app = new Vue({
+    el: '#app',
+    router,
+    components: {
+        Index
+    }
+});
